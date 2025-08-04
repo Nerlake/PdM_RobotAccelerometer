6f1449fc497586d3f80dda7af4d04bf280d2e29a
Léo HOTZ <cdf.leo.hotz@gmail.com>
2025-08-04 18:27:38 +0200

    Feat: Create CSV file for dataset

---
+++ Server/create_dataset.py
@@ -0,0 +1,34 @@
+import socket
+import os
+
+host = "0.0.0.0"
+port = 12345
+csv_path = "data/normal_data.csv"
+
+os.makedirs("data", exist_ok=True)
+
+if not os.path.exists(csv_path):
+    with open(csv_path, "w") as f:
+        f.write("accel_x,accel_y,accel_z,gyro_x,gyro_y,gyro_z,gforce\n")
+
+server_socket = socket.socket()
+server_socket.bind((host, port))
+server_socket.listen(1)
+
+conn, addr = server_socket.accept()
+
+with open(csv_path, "a") as f:
+    while True:
+        data = conn.recv(1024)
+        if not data:
+            break
+        line = data.decode().strip()
+
+        if line:
+            print("Reçu :", line)
+            f.write(line + "\n")
+            f.flush()
+
+conn.close()
+server_socket.close()
+print("Connection has been closed")
